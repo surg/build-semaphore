51c8ae91f433858c30ee8a08e13c9b5097526af9
Yury Lytvynenko <ortega.nav@gmail.com>
2013-07-04 00:22:31 +0300

    start jenkins poll and http server via single script

--- build_status.py
+++ build_status.py
@@ -4,6 +4,8 @@ from urllib import urlopen
 from datetime import datetime
 import threading
 import re
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 POLL_INTERVAL = 60
 
@@ -27,6 +29,21 @@ def read_config(config_file_name):
 	cfg = eval(config)
 	return cfg
 
+def start_server(config):
+	HandlerClass = SimpleHTTPRequestHandler
+	ServerClass  = BaseHTTPServer.HTTPServer
+	Protocol     = "HTTP/1.0"
+
+	port = int(config.get('port', 8000))
+	server_address = ('localhost', port)
+
+	HandlerClass.protocol_version = Protocol
+	httpd = ServerClass(server_address, HandlerClass)
+
+	sa = httpd.socket.getsockname()
+	print "Serving HTTP on", sa[0], "port", sa[1], "..."
+	httpd.serve_forever()
+
 config_file_name = 'config.py'
 if (len(sys.argv) > 1):
 	config_file_name = sys.argv[1]
@@ -34,3 +51,7 @@ config = read_config(config_file_name)
 
 # Start polling jenkins
 job_stats(config['jenkins'], config['jobs'])
+
+# Start simple http server to make html page available via http.
+# This is necessary mostly to allow jquery querying file in the same domain. 
+start_server(config)
